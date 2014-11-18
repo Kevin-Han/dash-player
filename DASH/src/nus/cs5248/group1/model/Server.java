8ed5abfd18062c949ce5959bbdcf1db169b8f7c9
pr.dragon.han <pr.dragon.han@a1e06505-3937-e35b-2917-688447fa74a0>
2014-11-18 08:45:47 +0000

    clear import file and settings

--- DASH/src/nus/cs5248/group1/model/Server.java
+++ DASH/src/nus/cs5248/group1/model/Server.java
@@ -1,11 +1,8 @@
 package nus.cs5248.group1.model;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,27 +28,13 @@ import org.apache.http.cookie.Cookie;
 import org.apache.http.cookie.CookieSpec;
 import org.apache.http.cookie.CookieSpecFactory;
 import org.apache.http.cookie.MalformedCookieException;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.cookie.CookieOrigin;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-
-import android.content.Intent;
 import android.net.ParseException;
-import android.os.AsyncTask;
 import android.util.Log;
-import android.webkit.URLUtil;
 
 public class Server
 {
@@ -116,7 +99,7 @@ public class Server
 
 			if (responseEntity != null)
 			{
-				this.responseAsText = EntityUtils.toString(responseEntity);
+				Server.responseAsText = EntityUtils.toString(responseEntity);
 				if (cookies != null)
 				{
 					int size = cookies.size();
